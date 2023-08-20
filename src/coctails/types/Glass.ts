a5da6c0aa111c8dd228ff19be2334d3a636fe9a3
Irina Zotova <zotovaiv87@gmail.com>
2023-08-20 18:31:01 -0400

    Add types

---
+++ src/coctails/types/Glass.ts
@@ -0,0 +1,16 @@
+export enum Glass {
+    NickAndNora = 'NickAndNora',
+    Champagne = 'Champagne',
+    Martini = 'Martini',
+    Collins = 'Collins',
+    Cup = 'Cup',
+    Goblet = 'Goblet',
+    Hightball = 'Hightball',
+    Hurracane = 'Hurracane',
+    Margarita = 'Margarita',
+    Beer = 'Beer',
+    Rock = 'Rock',
+    SingleRock = 'SingleRock',
+    Shooter = 'Shooter',
+    Tiki = 'Tiki'
+}
